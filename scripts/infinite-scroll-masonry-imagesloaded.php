607172973850d75b285dea41ce6ea1f129816ffa
Kristian <talleykristian@gmail.com>
2018-09-23 22:07:30 -0700

    completed basic directory architecture and gathered images ripped from old site.

---
+++ scripts/infinite-scroll-masonry-imagesloaded.php
@@ -0,0 +1,8 @@
+/**
+ * Be sure to include library scripts in this order. Can be placed either
+ * in the header or footer.
+ */
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-infinitescroll/2.0b2.120519/jquery.infinitescroll.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/masonry/3.1.2/masonry.pkgd.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.0.4/jquery.imagesloaded.min.js"></script>
